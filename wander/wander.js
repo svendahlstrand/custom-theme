193e9c0172555556d284d1159ace11c763d7c0e3
Sven Dahlstrand <sven@dahlstrand.net>
2026-03-18 11:15:49 +0100

    Add Wander with small sample of sites.

---
+++ wander/wander.js
@@ -0,0 +1,20 @@
+window.wander = {
+  // Websites and pages you recommend to other wanderers.
+  pages: [
+    'https://sannalund.se',
+    'https://100r.ca/site/home.html',
+    'https://wiki.xxiivv.com/site/home.html',
+    'https://kokorobot.ca/site/pics.html',
+    'https://cloudlord.management',
+    'https://elliott.computer',
+    'https://megmiller.world',
+    'https://laurelschwulst.com',
+    'https://html.energy',
+    'https://websitesite.xyz',
+    'https://dahlstrand.net/'
+  ],
+  // Other Wander consoles that visitors can reach from your console.
+  consoles: [
+    'https://susam.net/wander/'
+  ],
+}
